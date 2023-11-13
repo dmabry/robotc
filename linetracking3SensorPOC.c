eb8263f1a020cc07f5d79978f08bae9c886b4864
David Mabry <5330413+dmabry@users.noreply.github.com>
2023-11-13 09:48:51 -0600

    fixed formatting with robotc and tested in RVW with clawbot.

--- linetracking3SensorPOC.c
+++ linetracking3SensorPOC.c
@@ -7,42 +7,42 @@
 
 task main()
 {
-  // Wait 1 second before starting
-  wait1Msec(1000);
+	// Wait 1 second before starting
+	wait1Msec(1000);
 
-  // This threshold will have to be adjusted depending on the environment and
-  // the line being tracked.  You may also have to invert the comparisons if
-  // you are tracking a "dark" line instead of "light" line.
-  int threshold = 2400;
+	// This threshold will have to be adjusted depending on the environment and
+	// the line being tracked.  You may also have to invert the comparisons if
+	// you are tracking a "dark" line instead of "light" line.
+	int threshold = 2400;
 
-  // loopDelay controls how often the robot will check the sensor and align.
-  int loopDelay = 25;
+	// loopDelay controls how often the robot will check the sensor and align.
+	int loopDelay = 25;
 
-  // start line following
-  while(true)
-  {
-    // Right sensor trigger:
-    if(SensorValue(lineTrackerRight) > threshold)
-    {
-      // counter-steer right:
-      motor[leftMotor]  = 63;
-      motor[rightMotor] = 0;
-    }
-    // Center sensor trigger:
-    if(SensorValue(lineTrackerCenter) > threshold)
-    {
-      // go straight
-      motor[leftMotor]  = 63;
-      motor[rightMotor] = 63;
-    }
-    // Left sensor trigger:
-    if(SensorValue(lineTrackerLeft) > threshold)
-    {
-      // counter-steer left:
-      motor[leftMotor]  = 0;
-      motor[rightMotor] = 63;
-    }
-  }
-  wait1Msec(loopDelay);
+	// start line following
+	while(true)
+	{
+		// Right sensor trigger:
+		if(SensorValue(lineTrackerRight) > threshold)
+		{
+			// counter-steer right:
+			motor[leftMotor]  = 63;
+			motor[rightMotor] = 0;
+		}
+		// Center sensor trigger:
+		if(SensorValue(lineTrackerCenter) > threshold)
+		{
+			// go straight
+			motor[leftMotor]  = 63;
+			motor[rightMotor] = 63;
+		}
+		// Left sensor trigger:
+		if(SensorValue(lineTrackerLeft) > threshold)
+		{
+			// counter-steer left:
+			motor[leftMotor]  = 0;
+			motor[rightMotor] = 63;
+		}
+	}
+	wait1Msec(loopDelay);
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
